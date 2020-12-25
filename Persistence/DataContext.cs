29263044c177ccc1b6336ec17a17b85112b5576c
LDAP\dthoang <dthoang@cmc.com.vn>
2020-12-25 16:09:29 +0700

    Init Project

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
